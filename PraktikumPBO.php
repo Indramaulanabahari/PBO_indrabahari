86925a17a456ef7009c70c50b2c353fa0618a839
Indramaulanabahari <gansindra70@gmail.com>
2024-08-19 12:59:58 +0700

    Add files via upload

--- PraktikumPBO.php
+++ PraktikumPBO.php
@@ -1,21 +1,34 @@
 <?php
-$c = 10;
+$c = 10; // Suhu dalam Celcius
 
-function KeFahrennhit() {
+// Konversi Celcius ke Fahrenheit
+function KeFahrenheit() {
     global $c;
-    global $k;
-    $k = ($c * 9/5) + 32;
+    global $f;
+    $f = ($c * 9/5) + 32;
 }
 
+// Konversi Celcius ke Reamur
 function KeReamur() {
     global $c;
-    global $k;
-    $k = 4/5 * $c;
+    global $r;
+    $r = 4/5 * $c;
 }
 
+// Konversi Celcius ke Kelvin
 function KeKelvin() {
     global $c;
-    global $c;
+    global $k;
     $k = $c + 273.15;
 }
 
+// Panggil fungsi konversi
+KeFahrenheit();
+echo "<p>Celcius ke Fahrenheit: $f °F</p>";
+
+KeReamur();
+echo "<p>Celcius ke Reamur: $r °R</p>";
+
+KeKelvin();
+echo "<p>Celcius ke Kelvin: $k K</p>";
+?>
