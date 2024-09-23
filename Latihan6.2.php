7b0cd16551d37472bb3a81dfe147fe3731cc023c
Indramaulanabahari <gansindra70@gmail.com>
2024-09-23 09:33:16 +0700

    Add files via upload

---
+++ Latihan6.2.php
@@ -0,0 +1,16 @@
+<?php
+class Perulangan
+{
+    public function loop()
+    {
+        for ($i = 15; $i > 0; $i--) {
+            echo $i;
+            echo "<br/";
+        }
+    }
+}
+$ObjekPerulangan = new Perulangan();
+echo "Jenis Perulangan Lainnya" . "<br/>";
+echo $ObjekPerulangan->Loop() . "<br/>";
+
+?>
